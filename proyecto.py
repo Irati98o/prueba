ce0d48e70898bcd0104a8184c8892281df83a113
IratxeCampo <iratxe.campo@opendeusto.es>
2016-10-19 20:54:05 +0200

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -4,4 +4,4 @@ incremento = int(input("Introduce el incremento en cada paso del tiempo (s): "))
 while dias < 0 or incremento < 0:
 	print ("Vuelve a introducir los datos.")
 	dias = int(input("Introduce el tiempo total de la simulación (días): "))
-	incremento = int(input("Introduce el incremento en cada paso del tiempo (s): "))
+	incremento = int(input("Introduce el incremento en cada paso de tiempo (s): "))
