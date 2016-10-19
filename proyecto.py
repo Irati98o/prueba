5bbe9185971b36422c622404b05611811621c9e0
IratxeCampo <iratxe.campo@opendeusto.es>
2016-10-19 20:47:40 +0200

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,7 @@
+#Programa para hacer girar la luna entorno a la tierra
+dias = int(input("Introduce el tiempo total de la simulación (días): "))
+incremento = int(input("Introduce el incremento en cada paso del tiempo (s): "))
+while dias < 0 or incremento < 0:
+	print ("Vuelve a introducir los datos.")
+	dias = int(input("Introduce el tiempo total de la simulación (días): "))
+	incremento = int(input("Introduce el incremento en cada paso del tiempo (s): "))
